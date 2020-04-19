9769b47a144892c700a92fac60f17cde4189f0bf
a-afful <49870112+a-afful@users.noreply.github.com>
2020-04-19 19:44:21 -0400

    Add files via upload

--- coffeeBot.py
+++ coffeeBot.py
@@ -1,62 +1,63 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Apr 17 15:31:14 2020
+Created on Sun Apr 19 15:12:15 2020
 
 @author: abenaafful
 """
 
-def coffee_bot():
-  print("Welcome to the cafe!")
-  
-  size = get_size()
-  drink_type = get_drink_type()
-  print("Alright, that's a {} {}!".format(size, drink_type))
-  #print(size)
-  #print(drink_type)
-  name = input("Can I get your name please?\n")
-  print("Thanks, {}! Your drink will be ready shortly.".format(name))
+from utils import print_message, get_size, order_latte
 
-def get_size():
-  res = input('What size drink can I get for you? \n[a] Small \n[b] Medium \n[c] Large \n> ')
-  if res == 'a':
-    return "small"
-  elif res == 'b':
-    return "medium"
-  elif res == 'c':
-    return 'large'
-  else:
-    def print_message():
-      print("I'm sorry, I did not understand your selection. Please enter the corresponding letter for your response.")
-    print_message()
-    return get_size()
+def coffee_bot():
+  print('Welcome to the cafe!')
+  
+  order_drink = 'y'
+  drinks = []
+  
+  while order_drink == 'y':
+    size = get_size()  
+    drink_type = get_drink_type()
+    
+    drink = '{} {}'.format(size, drink_type)
+    print('Alright, that\'s a {}!'.format(drink))
+    drinks.append(drink)
+    
+  
+    while True:
+      order_drink = input("Would you like to order another drink? (y/n) \n>")
+      if order_drink in ['y', 'n']:
+        break
+  
+  print('Okay, so I have:')
+  for drink in drinks:
+    print('-', drink)
+    
+  name = input('Can I get your name please? \n> ')
+  print('Thanks, {}! Your order will be ready shortly.'.format(name))
 
 def get_drink_type():
-  res = input("What type of drink would you like? \n[a] Brewed Coffee \n[b] Mocha \n[c] Latte \n")
+  res = input('What type of drink would you like? \n[a] Brewed Coffee \n[b] Mocha \n[c] Latte \n> ')
+
   if res == 'a':
     return 'brewed coffee'
   elif res == 'b':
-    return 'mocha'
+    return order_mocha()
   elif res == 'c':
     return order_latte()
   else:
-    def print_message():
-      print("I'm sorry, I did not understand your selection. Please enter the corresponding letter for your response.")
     print_message()
     return get_drink_type()
   
-def order_latte():
-  res = input("And what kind of milk for your latte? \n[a] 2% milk \n[b] Non-fat milk \n[c] Soy milk \n")
-  if res == 'a':
-    return 'latte'
-  elif res == 'b':
-    return 'non-fat latte'
-  elif res == 'c':
-    return 'soy latte'
-  else:
-    def print_message():
-      print("I'm sorry, I did not understand your selection. Please enter the corresponding letter for your response.")
+# Define new functions here!
+def order_mocha():
+  while True:
+    res = input('Would you like to try our limited-edition peppermint mocha? \n[a] Sure! \n[b] Maybe next time! \n ')
+    
+    if res == 'a':
+      return 'peppermint mocha'
+    elif res == 'b':
+      return 'mocha'
+    
     print_message()
-    return get_drink_type()
 
 coffee_bot()
---
+++ utils.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 19 16:51:05 2020
+
+@author: abenaafful
+"""
+
+def print_message():
+  print('I\'m sorry, I did not understand your selection. Please enter the corresponding letter for your response.')
+  
+def get_size():
+  res = input('What size drink can I get for you? \n[a] Small \n[b] Medium \n[c] Large \n> ')
+  
+  if res == 'a':
+    return 'small'
+  elif res == 'b':
+    return 'medium'
+  elif res == 'c':
+    return 'large'
+  else:
+    print_message()
+    return get_size()
+
+def order_latte():
+  res = input('And what kind of milk for your latte? \n[a] 2% milk \n[b] Non-fat milk \n[c] Soy milk \n> ')
+
+  if res == 'a':
+    return 'latte'
+  elif res == 'b':
+    return 'non-fat latte'
+  elif res == 'c':
+    return 'soy latte'
+  else:
+    print_message()
+    return order_latte()
